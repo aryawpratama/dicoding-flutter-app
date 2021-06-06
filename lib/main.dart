4b55e5c12ad9fb81a45f9a53bd66bc1a140b3be7
aryawpratama <aryawpratama@gmail.com>
2021-06-06 15:15:28 +0700

    first-screen-practice

--- lib/main.dart
+++ lib/main.dart
@@ -13,59 +13,40 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
             primarySwatch: Colors.blue,
             visualDensity: VisualDensity.adaptivePlatformDensity),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Hello World'),
-          ),
-          body: Center(
-            child: BiggerText(
-              text: 'Hello World',
-            ),
-          ),
-        ));
+        home: FirstScreen());
   }
 }
 
-class Heading extends StatelessWidget {
-  final String text;
-
-  Heading({required this.text});
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-    );
-  }
-}
-
-class BiggerText extends StatefulWidget {
-  final String text;
-  const BiggerText({required this.text});
-  @override
-  _BiggerTextState createState() => _BiggerTextState();
-}
-
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          widget.text,
-          style: TextStyle(fontSize: _textSize),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          onPressed: () {},
         ),
-        ElevatedButton(
-            child: Text('Perbesar'),
-            onPressed: () {
-              setState(() {
-                _textSize = 32.0;
-              });
-            })
-      ],
+        title: Text('First Screen'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Center(
+        child: Text('Hello World'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
